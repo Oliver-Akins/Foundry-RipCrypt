5933473e4ac2dc004f28cd3eee24044654600d1e
Oliver-Akins <Oliver.akins1999@gmail.com>
2024-12-10 01:19:11 -0700

    Initialize the data model and register it

---
+++ module/data/Actor/Hero.mjs
@@ -0,0 +1,10 @@
+export class HeroData extends foundry.abstract.TypeDataModel {
+	static defineSchema() {
+		const schema = {};
+		return schema;
+	};
+
+	prepareBaseData() {};
+
+	prepareDerivedData() {};
+};
--- module/hooks/init.mjs
+++ module/hooks/init.mjs
@@ -1,5 +1,9 @@
+import { HeroData } from "../data/Actor/Hero.mjs";
 import { Logger } from "../utils/Logger.mjs";
 
 Hooks.once(`init`, () => {
 	Logger.log(`Initializing`);
+
+	// Datamodel registrations
+	CONFIG.Actor.dataModels.hero = HeroData;
 });
