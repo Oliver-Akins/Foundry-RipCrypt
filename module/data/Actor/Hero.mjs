c52777825128ee9bbb354557023fb20442b6fb40
Oliver-Akins <Oliver.akins1999@gmail.com>
2024-12-10 21:12:12 -0700

    RC-3 | Add Abilities data model structure

--- module/data/Actor/Hero.mjs
+++ module/data/Actor/Hero.mjs
@@ -1,6 +1,39 @@
+const { fields } = foundry.data;
+
 export class HeroData extends foundry.abstract.TypeDataModel {
 	static defineSchema() {
-		const schema = {};
+		const schema = new fields.SchemaField({
+			ability: new fields.SchemaField({
+				grit: new fields.NumberField({
+					min: 0,
+					initial: 1,
+					integer: true,
+					required: true,
+					nullable: false,
+				}),
+				gait: new fields.NumberField({
+					min: 0,
+					initial: 1,
+					integer: true,
+					required: true,
+					nullable: false,
+				}),
+				grip: new fields.NumberField({
+					min: 0,
+					initial: 1,
+					integer: true,
+					required: true,
+					nullable: false,
+				}),
+				glim: new fields.NumberField({
+					min: 0,
+					initial: 1,
+					integer: true,
+					required: true,
+					nullable: false,
+				}),
+			}),
+		});
 		return schema;
 	};
 
