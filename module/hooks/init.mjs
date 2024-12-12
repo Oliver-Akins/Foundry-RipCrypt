2fc3b867921baddb224423c9f4f333d8af651db9
Oliver-Akins <Oliver.akins1999@gmail.com>
2024-12-12 00:39:09 -0700

    Add dev settings for convenience

--- module/hooks/init.mjs
+++ module/hooks/init.mjs
@@ -1,9 +1,31 @@
+// Applications
+import { HeroSummaryCardV1 } from "../Apps/ActorSheets/HeroSummaryCardV1.mjs";
+
+// Data Models
 import { HeroData } from "../data/Actor/Hero.mjs";
+import { registerDevSettings } from "../settings/devSettings.mjs";
+
+// Misc
 import { Logger } from "../utils/Logger.mjs";
 
 Hooks.once(`init`, () => {
 	Logger.log(`Initializing`);
 
-	// Datamodel registrations
+	// #region Settings
+	registerDevSettings();
+	// #endregion
+
+	// #region Datamodels
 	CONFIG.Actor.dataModels.hero = HeroData;
+	// #endregion
+
+	// #region Sheets
+	// #region Actors
+	Actors.registerSheet(game.system.id, HeroSummaryCardV1, {
+		makeDefault: true,
+		types: [`hero`],
+		label: `RipCrypt.sheet-names.HeroSummaryCardV1`,
+	});
+	// #endregion
+	// #endregion
 });
--- module/hooks/ready.mjs
+++ module/hooks/ready.mjs
@@ -2,4 +2,18 @@ import { Logger } from "../utils/Logger.mjs";
 
 Hooks.once(`ready`, () => {
 	Logger.log(`Ready`);
+
+	let defaultTab = game.settings.get(`ripcrypt`, `defaultTab`);
+	if (defaultTab) {
+		if (!ui.sidebar?.tabs?.[defaultTab]) {
+			Logger.error(`Couldn't find a sidebar tab with ID:`, defaultTab);
+		} else {
+			Logger.debug(`Switching sidebar tab to:`, defaultTab);
+			ui.sidebar.tabs[defaultTab].activate();
+		};
+	};
+
+	if (game.settings.get(`ripcrypt`, `devMode`)) {
+		if (game.paused) { game.togglePause() };
+	};
 });
