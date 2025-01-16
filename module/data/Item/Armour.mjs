4948c7e755e50588a7cc74484d9abdb5b09f4d17
Oliver-Akins <Oliver.akins1999@gmail.com>
2025-01-15 19:51:31 -0700

    RC-102 | Armour | Protection

--- module/data/Item/Armour.mjs
+++ module/data/Item/Armour.mjs
@@ -1,9 +1,13 @@
+import { requiredInteger } from "../helpers.mjs";
+
 const { fields } = foundry.data;
 
 export class ArmourData extends foundry.abstract.TypeDataModel {
 	// MARK: Schema
 	static defineSchema() {
-		return {};
+		return {
+			protection: requiredInteger({ min: 0, initial: 1 }),
+		};
 	};
 
 	// MARK: Base Data
@@ -18,7 +22,15 @@ export class ArmourData extends foundry.abstract.TypeDataModel {
 
 	// #region Sheet Data
 	getFormFields(ctx) {
-		const fields = [];
+		const fields = [
+			{
+				type: `integer`,
+				label: `RipCrypt.common.protection`,
+				value: this.protection,
+				path: `system.protection`,
+				min: 0,
+			},
+		];
 		return fields;
 	};
 	// #endregion
